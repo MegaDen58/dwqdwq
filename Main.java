729b2fca956999f8cba46edcdee8ae8473221975
Denis <samparizona2018@yandex.ru>
2022-09-17 00:16:51 +0300

    vvvvv

--- Main.java
+++ Main.java
@@ -5,42 +5,30 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Start!!");
 
-        MyThread myThread1 = new MyThread();
-        myThread1.start();
+        AnimalThread rabbit = new AnimalThread("Rabbit", 5);
+        rabbit.start();
 
-        MyThread myThread2 = new MyThread();
-        myThread2.start();
-
-        MyThread myThread3 = new MyThread();
-        myThread3.start();
-
-        MyThread myThread4 = new MyThread();
-        myThread4.start();
-
-        MyThread myThread5 = new MyThread();
-        myThread5.start();
-
-        MyThread myThread6 = new MyThread();
-        myThread6.start();
-
-        MyThread myThread7 = new MyThread();
-        myThread7.start();
-
-        MyThread myThread8 = new MyThread();
-        myThread8.start();
-
-        MyThread myThread9 = new MyThread();
-        myThread9.start();
-
-        MyThread myThread10 = new MyThread();
-        myThread10.start();
+        AnimalThread turtle = new AnimalThread("Turtle", 5);
+        turtle.start();
     }
 }
 
-class MyThread extends Thread{
+class AnimalThread extends Thread{
+    String animalName;
+    int counter;
+
+    AnimalThread(String name, int priority){
+        animalName = name;
+        this.setPriority(priority);
+    }
+
     public void run(){
-        for(int i = 0; i < 20; i++){
-            System.out.println(getName());
+        for(int i = 0; i <= 1000; i += 100){
+            this.counter = i;
+            if(animalName.equals("Rabbit") && i == 700){
+                this.setPriority(1);
+            }
+                System.out.println(animalName + " " + i);
         }
     }
 }
